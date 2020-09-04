3849950cff022c4778b64064502f66815dda18c9
Heedong Kim <ruthetum@gmail.com>
2020-09-05 00:37:34 +0900

    분할된 음성 파일에서 스크립트 추출

--- stt.py
+++ stt.py
@@ -1,12 +1,12 @@
 from google.cloud import speech_v1
 from google.cloud.speech_v1 import enums
 
-def audio_recognize(storage_uri):
+def audio_recognize(storage_uri, index):
 
     client = speech_v1.SpeechClient()
 
     # 인코딩한 hertz 따라 값 변경
-    sample_rate_hertz = 44100
+    # sample_rate_hertz = 44100
     
     language_code = "en-US"
     # language_code = "en-IN"
@@ -14,19 +14,17 @@ def audio_recognize(storage_uri):
     encoding = enums.RecognitionConfig.AudioEncoding.LINEAR16
     # 오디오 채널 옵션은 영상에 따라 유무 선택
     config = {
-        "sample_rate_hertz": sample_rate_hertz,
+        # "sample_rate_hertz": sample_rate_hertz,
         "language_code": language_code,
         "encoding": encoding,
         "audio_channel_count" : 2,
     }
     audio = {"uri": storage_uri}
 
-    print(u"Waiting for operation to complete...")
+    print(u"[ "+ str(index) +"% ]Waiting for operation to complete...")
 
     operation = client.long_running_recognize(config, audio)
     response = operation.result()
-
-    print("1")
 
     script = ""
     for result in response.results:
@@ -35,7 +33,13 @@ def audio_recognize(storage_uri):
         print(u"Transcript: {}".format(alternative.transcript))
         script = script + alternative.transcript + "\n"
     
-    with open("./en-scripts/script.txt", "w") as sc:
+    with open("./en-scripts/test-audio/script.txt", "a") as sc:
             sc.write(script)
 
-audio_recognize("gs://fornawab/ssmovie.wav")
+def main():
+    f = open("./en-scripts/test-audio/script.txt", 'w')
+    f.close()
+    for i in range(1,10):
+        audio_recognize("gs://fornawab/test-audio/part" + str(i) +".wav", i*10)
+
+main()
